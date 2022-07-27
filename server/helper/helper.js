df7180e5190a5128dbfa51c59e88507731a2d244
Joaquin <joacosanchez20@gmail.com>
2022-07-27 19:26:07 -0300

    change auth

--- server/helper/helper.js
+++ server/helper/helper.js
@@ -1,5 +1,10 @@
 const sendRefreshToken = (res, token) => {
-    res.cookie('refreshtoken', token)
+    res.cookie('refreshtoken', token, {
+        httpOnly: true, 
+        secure: true, 
+        sameSite: 'none',
+        path: '/'
+    })
 }
 
 const sendAccessToken = (res, id, username, token) => {
