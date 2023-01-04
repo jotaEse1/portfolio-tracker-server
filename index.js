55baf2f509f07b750dfac9d885c742a6b4c370c1
Joaquin <joacosanchez20@gmail.com>
2023-01-04 18:46:33 -0300

    option code

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ connection.connect(err => {
 })
 
 const allowCors = fn => async (req, res) => {
-    // res.setHeader('Access-Control-Allow-Credentials', true)
-    // res.setHeader('Access-Control-Allow-Origin', '*')
-    // // another common pattern
-    // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    // res.setHeader(
-    //   'Access-Control-Allow-Headers',
-    //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    // )
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
     if (req.method === 'OPTIONS') {
       res.status(200).end()
       return
@@ -37,10 +37,10 @@ const allowCors = fn => async (req, res) => {
   
 
 //middlewares
-app.use(cors({
-    origin: "https://jotaese1.github.io",
-    credentials: true
-}))
+// app.use(cors({
+//     origin: "https://jotaese1.github.io",
+//     credentials: true
+// }))
 app.use(cookieParser())
 app.use(express.json({limit: '500mb'}))
 
