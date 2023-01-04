9e75cdeb14744b1c8e85511fcac47e46d04c773b
Joaquin <joacosanchez20@gmail.com>
2023-01-04 15:50:15 -0300

    cors no

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ connection.connect(err => {
 })
 
 //middlewares
-app.use(cors({
-    origin: 'https://jotaese1.github.io',
-    credentials: true
-}))
+// app.use(cors({
+//     origin: 'https://jotaese1.github.io',
+//     credentials: true
+// }))
 app.use(cookieParser())
 app.use(express.json({limit: '500mb'}))
 
@@ -42,26 +42,3 @@ app.listen(port, (req, res) => {
     console.log(`Server is running on port ${port}`)
 })
 
-const allowCors = fn => async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    if (req.method === 'OPTIONS') {
-      res.status(200).end()
-      return
-    }
-    return await fn(req, res)
-  }
-  
-  const handler = (req, res) => {
-    const d = new Date()
-    res.end(d.toString())
-  }
-  
-  module.exports = allowCors(handler)
